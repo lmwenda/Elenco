97362cd2bc3ca015b9fa521e2de59523fd356f7e
lMwenda <Lukemwen@hotmail.com>
2022-05-26 21:02:21 +0100

    ready for production

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
 import Image from 'next/image';
 import React from 'react';
 import { motion } from 'framer-motion';
-import image from "../public/assets/Checklist.jpg";
+import image from '../public/assets/Checklist.jpg';
 import Typewriter, { TypewriterClass } from 'typewriter-effect';
 import Link from 'next/link';
 
-
 const landingPage = () => {
   return (
-    <motion.section animate={{ x: [ -75, 0 ], opacity: [ 0, 0.5, 0.7, 0.9, 1 ] }} transition={{ ease: "easeIn" }}>
+    <motion.section animate={{ x: [ -75, 0 ], opacity: [ 0, 0.5, 0.7, 0.9, 1 ] }} transition={{ ease: 'easeIn' }}>
       <div
-        className="container flex flex-col-reverse items-center px-6 mx-auto mt-1 space-y-12 md:space-y-0 md:mt-20 md:flex-row"
+        className='container flex flex-col-reverse items-center px-6 mx-auto mt-1 space-y-12 md:space-y-0 md:mt-20 md:flex-row'
       >
-          <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
+          <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
             <h1
-                className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left"
+                className='max-w-md text-4xl font-bold text-center md:text-5xl md:text-left'
             >
               <Typewriter
                 onInit={(typewriter: TypewriterClass) => {
@@ -25,67 +24,70 @@ const landingPage = () => {
               />
             </h1>
 
-            <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
-                Ever lost track on your tasks? 
-                Forgot what you planned on doing today? 
-                Other Management Apps to slow for you? You've come to the right place... 
-            </p>
-            <div className="flex justify-center md:justify-start">
+            <div> 
+              <p className='max-w-sm text-center text-darkGrayishBlue md:text-left'>
+                  Ever lost track on your tasks? 
+                  Forgot what you planned on doing today? 
+                  Other Management Apps to slow for you? You&apos;ve come to the right place... 
+              </p>
+            </div>    
+
+            <div className='flex justify-center md:justify-start'>
                 <motion.div
                 animate={{ y: [ 0, -10, 0 ], opacity: 1 }}
                 transition={{
                   delay: 1,
-                  ease: "easeInOut",
-                  y: { type: "bounce", stiffness: 15, repeat: Infinity },
+                  ease: 'easeInOut',
+                  y: { type: 'bounce', stiffness: 15, repeat: Infinity },
                   default: { duration: 2, repeat: Infinity },
                 }}
-                className="mt-5">
+                className='mt-5'>
                   <Link href='/todos'> 
-                    <button className="bg-red-500 text-white px-6 p-3 hover:opacity-75">New Task</button>
+                    <button className='bg-red-500 text-white px-6 p-3 hover:opacity-75'>New Task</button>
                   </Link>
                 </motion.div>
             </div>
           </div>
 
           <motion.div 
-            className="md:w-1/2"
+            className='md:w-1/2'
             animate={{ opacity: 1, y: [ 0, -50]}}
             transition={
               {
                 delay: 1,
-                y: { type: "bounce", stiffness: 15 },
-                default: { duration: 3, repeatType: "loop", repeat: Infinity },
+                y: { type: 'bounce', stiffness: 15 },
+                default: { duration: 3, repeatType: 'loop', repeat: Infinity },
               }}
             >
-              <Image className="sm:w-auto md:w-9/12" height={0} width={0} src={image} alt="" />
+              <Image className='sm:w-auto md:w-9/12' height={0} width={0} src={image} alt='' />
           </motion.div>
       </div>
 
-      <div className="relative container mt-14 flex items-center px-6 mx-auto space-y-10 justify-center flex-col">
-          <h1 className="text-3xl text-center">
+      <div className='relative container mt-14 flex items-center px-6 mx-auto space-y-10 justify-center flex-col'>
+          <h1 className='text-3xl text-center'>
               Our products is used by teams from around the world
           </h1>
 
-          <div className="baseline flex flex-col space-y-10 items-center justify-center md:space-y-0 md:flex-row md:space-x-5">
-              <img src="
+          <div className='baseline flex flex-col space-y-10 items-center justify-center md:space-y-0 md:flex-row md:space-x-5'>
+              <img src='
               https://assets.website-files.com/6085e38567384555aea9df90/60e2756704be844efefe70a5_Carded%20Black%20Logo.svg
-              " alt="" />
+              ' alt='' />
 
-              <img src="
+              <img src='
               https://assets.website-files.com/6085e38567384555aea9df90/60e27567d03c58ecd9f102dc_Emerald%20Black%20Logo.svg
-              " alt="" />
+              ' alt='' />
 
-              <img src="
+              <img src='
               https://assets.website-files.com/6085e38567384555aea9df90/60e275675067bd7021d81225_Etro%20Black%20Logo.svg
-              " alt="" />
+              ' alt='' />
 
-              <img src="
+              <img src='
               https://assets.website-files.com/6085e38567384555aea9df90/60e27567d03c58eddcf102db_Focusfox%20Black%20Logo.svg
-              " alt="" />
+              ' alt='' />
 
-              <img src="
+              <img src='
               https://assets.website-files.com/6085e38567384555aea9df90/60e275679137b9062facb23d_Optimer%20Black%20Logo.svg
-              " alt="" />
+              ' alt='' />
               
           </div>
       </div>
--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -1,16 +1,15 @@
-import cx from "classnames";
+import cx from 'classnames';
 import { uid } from 'uid';
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import Todo from "../../components/Todo";
-import { NextPage } from "next";
+import Todo from '../../components/Todo';
 
-const index: NextPage<any> = () => {
-    const [todoItem, setTodoItem] = useState("");
+const Index = () => {
+    const [todoItem, setTodoItem] = useState('');
     const [items, setItems] = useState([]);
 
     const saveTasks = () => {
-        localStorage.setItem("todos", JSON.stringify(items));
+        localStorage.setItem('todos', JSON.stringify(items));
     }
 
     const handleAdd = (e: React.FormEvent) => {
@@ -25,7 +24,7 @@ const index: NextPage<any> = () => {
             ...items,
         ]);
 
-        setTodoItem("");
+        setTodoItem('');
         }
     };
 
@@ -45,38 +44,38 @@ const index: NextPage<any> = () => {
     };
 
     useEffect(() => {
-        const _items = localStorage.getItem("todos");
+        const _items = localStorage.getItem('todos');
         if(_items) {
             setItems(JSON.parse(_items));
         }
     }, [])
 
     return (
-        <motion.section animate={{ x: [ -75, 0 ], opacity: [ 0, 0.5, 0.7, 0.9, 1 ] }} transition={{ ease: "easeIn" }}>
+        <motion.section animate={{ x: [ -75, 0 ], opacity: [ 0, 0.5, 0.7, 0.9, 1 ] }} transition={{ ease: 'easeIn' }}>
         <div
-        className="container flex flex-col justify-center items-center px-6 mx-auto mt-1 space-y-12 md:space-y-5 md:mt-20"
+        className='container flex flex-col justify-center items-center px-6 mx-auto mt-1 space-y-12 md:space-y-5 md:mt-20'
         >   
-            <div className="flex flex-col space-y-10">
-                <h1 className="text-5xl text-bold text-center">Todo List</h1>
-                <form className="mt-10 flex flex-row space-x-1">
+            <div className='flex flex-col space-y-10'>
+                <h1 className='text-5xl text-bold text-center'>Todo List</h1>
+                <form className='mt-10 flex flex-row space-x-1'>
 
-                    <input className="border border-gray-500 rounded-full" onChange={
+                    <input className='border border-gray-500 rounded-full' onChange={
                         (e: React.ChangeEvent<HTMLInputElement>) => setTodoItem(e.target.value)
-                    } type="text" placeholder="Task:" />
+                    } type='text' placeholder='Task:' />
 
-                    <button className="p-3 w-20 rounded-full text-white bg-red-500" onClick={handleAdd}>Add</button>
+                    <button className='p-3 w-20 rounded-full text-white bg-red-500' onClick={handleAdd}>Add</button>
                 
                 </form>
             </div>
 
-            <div className="pt-10 flex flex-col space-y-10">
+            <div className='pt-10 flex flex-col space-y-10'>
                 {
                     items.filter((todoItem) => !todoItem.task_complete).map((todoItem) => {
                         return(
                             <button 
                             key={todoItem.id}
                             onClick={() => handleDone(todoItem.id)}
-                            className={cx("item", { done: todoItem.task_complete })}>
+                            className={cx('item', { done: todoItem.task_complete })}>
                                 <Todo key={todoItem.id} task_name={todoItem.task_name} task_complete={todoItem.task_complete} />
                             </button>
                         )
@@ -89,7 +88,7 @@ const index: NextPage<any> = () => {
                             <button 
                             key={todo.id}
                             onClick={() => handleDone(todo.id)}
-                            className={cx("item", { done: todo.task_complete })}>
+                            className={cx('item', { done: todo.task_complete })}>
                                 <Todo key={todo.id} task_name={todo.task_name} task_complete={todo.task_complete} />
                             </button>
                         )
@@ -97,12 +96,12 @@ const index: NextPage<any> = () => {
                 }
             </div>
 
-            <div className="mx-auto">
-                <button onClick={saveTasks} className="border border-red-500 text-red-500 px-6 p-3 hover:opacity-75">Save Tasks</button>
+            <div className='mx-auto'>
+                <button onClick={saveTasks} className='border border-red-500 text-red-500 px-6 p-3 hover:opacity-75'>Save Tasks</button>
             </div>
         </div>
     </motion.section>
     )
 }
 
-export default index;
+export default Index;
